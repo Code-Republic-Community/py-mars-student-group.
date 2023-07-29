09ecd1e3e15a7bd40e5400577efe3882971d7534
Eleonora <eleonorabdoyan988@gmail.com>
2023-07-29 13:11:44 +0400

    in file_handler.py save_address_book and load_address_book methods were changed to handle error in json file

--- AddressBook/address_book/file_handler.py
+++ AddressBook/address_book/file_handler.py
@@ -15,34 +15,41 @@ class FileHandler:
         """
         Saves the address book to a JSON file.
 
-            Parameters:
-                my_address_book (AddressBook): The AddressBook object representing the address book to be saved.
+        Parameters:
+            my_address_book (AddressBook): The AddressBook object representing the address book to be saved.
 
-            The address book is saved as a JSON formatted stream to the file specified by the path "data/address_book_data.json".
-            Each contact in the address book is represented as a dictionary with the contact's telephone number as the key
-            and a list of contact details (first name, middle name, last name, birthday, and email) as the value.
+        Description:
+            This method takes an AddressBook object and serializes its contacts to a JSON formatted file. The contacts
+            are represented as lists of contact details, including first name, middle name, last name, birthday, telephone
+            number, and email. The serialized data is written to the file specified by the path "data/address_book_data.json".
         """
         with open(r"data\address_book_data.json", "w") as f:
-            for contact in my_address_book.contacts:
-                json.dump({contact.tel_number: [contact.first_name, contact.middle_name, contact.last_name, contact.birthday, contact.email]}, f)
-                f.write("\n")
+            json.dump([
+                [contact.first_name,
+                 contact.middle_name,
+                 contact.last_name,
+                 contact.birthday,
+                 contact.tel_number,
+                 contact.email]
+                for contact in my_address_book.contacts], f, indent=2)
 
     def load_address_book(self, my_address_book):
         """
-            Loads the address book from a JSON file.
+        Loads the address book from a JSON file.
 
-            Parameters:
-                my_address_book (AddressBook): The AddressBook object where the loaded contacts will be stored.
+        Parameters:
+            my_address_book (AddressBook): The AddressBook object where the loaded contacts will be stored.
 
-            The address book is loaded from the JSON file specified by the path "data/address_book_data.json".
-            Each line in the JSON file represents a contact in the address book, serialized as a JSON object.
-            The contact details are extracted from each JSON object and a new Contact object is created and added to the address book.
+        Description:
+            This method reads data from a JSON file specified by the path "data/address_book_data.json".
+            Each contact in the file is represented as a list of contact details, including first name, middle name,
+            last name, birthday, telephone number, and email. The method deserializes the data and creates Contact objects
+            using the contact details. These Contact objects are then added to the provided AddressBook object.
         """
         with open(r"data\address_book_data.json", "r") as f:
-            for line in f:
-                data = json.loads(line)
-                for k, v in data.items():
-                    contact = Contact(v[0], v[1], v[2], v[3], k, v[4])
-                    my_address_book.contacts.append(contact)
+            data = json.load(f)
+            for contact_info in data:
+                my_address_book.contacts.append(Contact(*contact_info))
+
 
 
